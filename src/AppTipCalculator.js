183e4a3856b85559d647f0ed1f09cce37a82da09
anotcat <hua19930930@gmail.com>
2024-10-21 16:30:20 +0800

    update calculate function

--- src/AppTipCalculator.js
+++ src/AppTipCalculator.js
@@ -10,36 +10,48 @@ export default function AppTipCalculator() {
 }
 
 function TipCalculator() {
+    const [bill, setBill] = useState("")
+    const [yourSelect, setYourSelect] = useState(0)
+    const [friendSelect, setFriendSelect] = useState(0)
+
+    function handleReset() {
+        setBill("")
+        setYourSelect(0)
+        setFriendSelect(0)
+    }
+
     return (
         <div>
-            <BillInput/>
-            <SelectPercentage title="How did you like the service?">
+            <BillInput bill={bill} onSetBill={setBill}/>
+            <SelectPercentage title="How did you like the service?" setSelect={yourSelect} onSetSelect={setYourSelect}>
 
             </SelectPercentage>
-            <SelectPercentage title="How did your friend like the service?">
+            <SelectPercentage title="How did your friend like the service?" setSelect={friendSelect}
+                              onSetSelect={setFriendSelect}>
 
             </SelectPercentage>
-            <Output/>
-            <Reset/>
+            <Output bill={bill} yourSelect={yourSelect} friendSelect={friendSelect}/>
+            <Reset onReset={handleReset}/>
         </div>
     );
 }
 
-function BillInput() {
+function BillInput({bill, onSetBill}) {
     return (
         <div>
             <label>How much was the bill?</label>
-            <input type="text" placeholder="Bill value"/>
+            <input type="text" placeholder="Bill value" value={bill} onChange={e => onSetBill(Number(e.target.value))}/>
         </div>
     )
 }
 
-function SelectPercentage({title}) {
+function SelectPercentage({title, setSelect, onSetSelect}) {
+
     return (
         <div>
             <p>
                 <label>{title}</label>
-                <select value="0">
+                <select value={setSelect} onChange={e => onSetSelect(Number(e.target.value))}>
                     <option value="0">Dissatisfied (0%)</option>
                     <option value="5">It was okay (5%)</option>
                     <option value="10">It was good (10%)</option>
@@ -50,18 +62,20 @@ function SelectPercentage({title}) {
     )
 }
 
-function Output() {
+function Output({bill, yourSelect, friendSelect}) {
+    const tip = bill * (yourSelect + friendSelect) * 0.01 * 0.5
+    const pay = bill + tip
     return (
         <div>
-            <h2>You pay $82 ($80 + $12 tip)</h2>
+            <h2>You pay ${pay} (${bill} + ${tip} tip)</h2>
         </div>
     )
 }
 
-function Reset() {
+function Reset({onReset}) {
     return (
         <div>
-            <button value="Reset">Reset</button>
+            <button onClick={onReset}>Reset</button>
         </div>
     )
 }
