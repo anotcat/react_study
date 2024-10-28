f205c376a5bca912dedd3688c00ed39db6748130
babala <hua19930930@gmail.com>
2024-10-28 18:30:18 +0800

    Add AppUsePopcorn

--- src/index.js
+++ src/index.js
@@ -3,16 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import AppAccordion from './AppAccordion';
 // import AppTipCalculator from './AppTipCalculator'
-import AppEatAndSplit from './AppEatAndSplit'
+// import AppEatAndSplit from './AppEatAndSplit'
+import AppUsePopcorn from "./AppUsePopcorn";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    {/*<AppAccordion />*/}
-    {/*  <AppTipCalculator />*/}
-      <AppEatAndSplit />
-  </React.StrictMode>
+    <React.StrictMode>
+        {/*<AppAccordion />*/}
+        {/*  <AppTipCalculator />*/}
+        {/*  <AppEatAndSplit />*/}
+        <AppUsePopcorn/>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
